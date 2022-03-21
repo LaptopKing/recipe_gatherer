61179784f3ecf4be5700296e522a039c5febd3ad
Peter <40338605+LaptopKing@users.noreply.github.com>
2022-03-21 12:42:59 +0100

    Update main.py
    Made it OOP and saved list of urls to global variable.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import urllib.request
 import time
 
+
 class GetRecipeSites:
     def __init__(self):
-        page_id = 1
-        recipes_urls = []
+        self.page_id = 1
+        self.recipes_urls = []
+        self.base_url = "https://www.nosalty.hu/receptek?page="
+        self.page_count = 10
+
     def get_urls(self, r):
         response = str(r.read())
         resp = response.strip().replace("\\n", "").split('<')
@@ -22,20 +26,28 @@ class GetRecipeSites:
                 self.recipes_urls.append("https://www.nosalty.hu" + temp[3].split('"')[1])
 
     def run(self):
-        begin = time.time()
-        while (self.page_id != 100):
-            r = urllib.request.urlopen(f"https://www.nosalty.hu/receptek?page={str(page_id)}")
+        self.begin = time.time()
+        while (self.page_id != self.page_count):
+            r = urllib.request.urlopen(f"{self.base_url}{str(self.page_id)}")
             self.get_urls(r)
             self.page_id += 1
-        end = time.time()
+        self.end = time.time()
 
     def display(self):
         for url in self.recipes_urls:
             print(url)
+
     def check_runtime(self):
         print(f"Time taken to run the program {self.end - self.begin}")
 
+    def get_back_list_of_urls(self):
+        return self.recipes_urls
+class GetRecipeIngredients:
+    def __init__(self):
+        pass
+
 recipes = GetRecipeSites()
 recipes.run()
-recipes.display()
+# recipes.display()
 recipes.check_runtime()
+recipes_urls = recipes.get_back_list_of_urls()
